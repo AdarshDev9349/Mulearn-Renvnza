ccb3343de5e0179e6279f3e0593d0ab742ea16c2
Adarshdev <adarshdev9349@gmail.com>
2024-05-31 19:54:43 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
        <a href="/" className="nav-logo  no-space"><img src={logo} alt="Logo" /></a>
         <a href="/" className="nav-logo no-space mulearn"><img src={logo2} alt="Logo" /></a>
         </div>
-      <p><i className="fas fa-copyright"></i>  Made With Passion by MulearnUcek</p>
+      <p style={{margin:'8px'}}><i className="fas fa-copyright"></i>  Made With Passion by MulearnUcek</p>
       <p style={{margin:'10px'}} className="p2">All images, videos, and contents are property of riot games</p>
     </footer>
   );
