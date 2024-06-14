b4d32ddfec48d4adb1b42662c04c9b563e6f99e0
Adarshdev <adarshdev9349@gmail.com>
2024-06-14 22:15:19 +0530

    minor changes

--- src/components/Events.js
+++ src/components/Events.js
@@ -37,49 +37,49 @@ const Events = () => {
       <div className="main">
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
-            <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
+            <h2>Nexus Conclave</h2>
+            <p>Join Nexus Conclave 2024 at RenVnza to explore HR trends and tech community insights on "Navigating the Future of Work." Network and learn with industry leaders.</p>
             <Link to={'/event/6'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
-            <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
+            <h2>UI/UX Workshop</h2>
+            <p>Join our Mulearn UCEK workshop at RenVnzA'24 to master user-centered design, wireframing, and prototyping tools.</p>
             <Link to={'/event/4'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
-            <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
+            <h2>Product Designing</h2>
+            <p>Join the Product Designing Workshop at RenVnzA'24 by Mulearn UCEK and explore product development from ideation to final concepts!</p>
             <Link to={'/event/2'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card highlight">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
+            <h2>Dev Expo</h2>
             <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
             <Link to={'/event/1'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
+            <h2>Grand Quest Auto</h2>
             <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
             <Link to={'/event/3'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
+            <h2>3D Printing</h2>
             <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
             <Link to={'/event/5'}><button className="button">Learn More</button></Link>
           </div>
         </div>
         <div className="event-card">
           <div className="event-card-content">
-            <h2>Upcoming Event</h2>
+            <h2>PCB Designing</h2>
             <p>Join us for an exciting tech tournament featuring top players from around the world.</p>
             <Link to={'/event/7'}><button className="button">Learn More</button></Link>
           </div>
