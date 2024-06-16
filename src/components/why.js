dc642b4226b7af7d354b4f7c25405c9551df91da
Adarshdev <adarshdev9349@gmail.com>
2024-06-16 14:15:38 +0530

    minor changes

--- src/components/why.js
+++ src/components/why.js
@@ -13,6 +13,7 @@ const Partner = () => {
                 <p style={{marginBottom:'10px'}}>
                 MuLearn UCEK, supported by G TECH and over 250 multinational companies, offers an unparalleled platform for events and opportunities. Here’s why sponsoring us can make a significant difference:
                 </p>
+                <div className='small-div'>
                 <ul>
                     <li>Top 100 coders runner up</li>
                     <li>5+ students joined startup companies</li>
@@ -20,6 +21,7 @@ const Partner = () => {
                     <li>Ranked 8th in Kerala</li>
                     <li>500+ students skilled up</li>
                 </ul>
+                <img src={image} alt="Image" className="small-img" /></div>
                 <p>
                 We are partnering with the Kerala Startup Mission (KSUM) and are an integral part of the MuLearn Foundation. These collaborations enable us to provide top-notch resources and opportunities for our students, fostering innovation, skill development, and career advancement.
                 </p>
