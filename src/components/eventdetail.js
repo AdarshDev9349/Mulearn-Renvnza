9d6b16c2da6685b1815f2fc183f8dbf474a3491c
Adarshdev <adarshdev9349@gmail.com>
2024-06-25 23:56:39 +0530

    minor changes

--- src/components/eventdetail.js
+++ src/components/eventdetail.js
@@ -10,11 +10,12 @@ const eventDetails = {
   laser_engraving: {
     title: "Laser Engraving",
     description:
-      "Join us for an exciting hands-on workshop where you'll learn the fundamentals of 2D designing and fabrication using a laser cutter. This workshop is designed for beginners and enthusiasts who are keen to explore the creative and technical aspects of laser cutting technology. Whether you're a hobbyist, a designer, or simply curious about digital fabrication, this workshop will equip you with the skills and knowledge to bring your ideas to life.",
+      "Explore a 5-hour hands-on workshop focusing on the fundamentals of 2D designing and fabrication using a laser cutter. Ideal for beginners and enthusiasts, this workshop covers both the creative and technical aspects of laser cutting technology. Whether you're a hobbyist, designer, or simply curious about digital fabrication, you'll gain practical skills in software like Inkscape and hands-on experience with laser cutting machines. By the end of the workshop, you'll have the knowledge and skills to create and fabricate your own projects using 2D design techniques.Please note, no food will be provided.",
     image: image,
+    note:"",
     date: "3/07/2024",
     time: "10.00 AM",
-    venue: "University College of Engineering,Kariavattom",
+    venue: "Fab Lab Trivandrum",
   },
   product_designing: {
     title: "Product Designing",
@@ -49,11 +50,11 @@ const eventDetails = {
   "3d_printing": {
     title: "3D Printing Workshop",
     description:
-      "Join us for an engaging 3D Printing Workshop where you will explore the fundamentals of 3D printing technology. This workshop covers essential concepts, from designing 3D models to operating 3D printers. Gain hands-on experience with industry-standard software and hardware, and learn best practices for successful 3D prints. Whether you're a beginner or seeking to enhance your skills, this workshop will provide the insights and techniques needed to turn your creative ideas into physical objects. Take advantage of this opportunity to delve into the exciting world of 3D printing and elevate your design capabilities!",
+      "Attend an engaging 3D Printing Workshop where you'll delve into the fundamentals of this transformative technology. Day 1 introduces Fab Labs and Mini Fab Labs in our country, and covers 3D designing using software like Rhino. Day 2 offers hands-on training with industry-standard 3D printers, enabling you to bring your designs to life. Don't miss this chance to explore the exciting world of 3D printing and enhance your design capabilities! Please note, no food will be provided.",
     image: image,
     date: "01-02 July 2024",
     time: "10.00 AM",
-    venue: "University College of Engineering,Kariavattom",
+    venue: "Fab Lab Trivandrum",
   },
   nexus_conclave: {
     title: "Nexus Conclave",
@@ -67,11 +68,11 @@ const eventDetails = {
   pcb_designing: {
     title: "PCB Designing",
     description:
-      "Join us for an intensive PCB Designing Workshop where you'll learn to create schematics and develop your first fabricated PCB board. This hands-on session covers essential design principles, ideation steps, and best practices for PCB development. Gain practical experience in electronic design automation (EDA) tools and understand the complete workflow from concept to fabrication. Whether you're a beginner or looking to refine your skills, this workshop will equip you with the knowledge and confidence to bring your electronic projects to life.",
+      "Discover an intensive PCB Designing Workshop where you'll learn to create schematics and develop your first PCB board. This hands-on session covers essential design principles, ideation steps, and best practices for PCB development. Gain practical experience in EDA tools and understand the workflow from concept to fabrication. Perfect for beginners and those looking to refine their skills.Please note, no food will be provided.",
     image: image,
     date: "04-05 July 2024",
     time: "10.00 AM",
-    venue: "University College of Engineering,Kariavattom",
+    venue: "Fab Lab Trivandrum",
   },
   
 };
